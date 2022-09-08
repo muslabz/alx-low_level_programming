f127f769f463693181a4533f3c12b1a592be1091
muslabz <ajetunmobimust3@gmail.com>
2022-09-08 05:03:33 -0700

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,2 @@
-#include <stdio> 
-/** 
-*main _entry point
-*
-*Return 0 after printing the function
-*/
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle,");
-        return (0);
-}
+#!/bin/bash
+{puts("\"Programming is like building a multilingual puzzle,"); return (0);
