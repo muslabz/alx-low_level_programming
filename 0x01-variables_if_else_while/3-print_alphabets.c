01b4a7d280cbfa30572084f73e0e5cd449925000
muslabz <ajetunmobimust3@gmail.com>
2022-09-10 04:51:03 -0700

    h

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic in lower and upper case
- *
- * Return: Always (Success)
- */
+  * main - Prints the alphabetic in lower and upper case
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 { 
 	char c;
