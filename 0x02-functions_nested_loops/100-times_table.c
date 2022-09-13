52ecf9d798176f4c9606a7eaeddeac63da911e86
muslabz <ajetunmobimust3@gmail.com>
2022-09-13 09:14:21 -0700

    f

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,8 @@
 #include"main.h"
+
  /**
-  * print_times_table - prints time table of n
+  * print_times_table 
+  * prints time table of n
   * @n: takes number input
   */
 void print_times_table(int n)
