80f5703055c0591a2c4b7fed79cc8c4cfe981ade
muslabz <ajetunmobimust3@gmail.com>
2022-09-13 03:48:00 -0700

    s

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * jack_bauer - print every minute of the day
+ *
+ * Return: every minute of the day of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	int H, h, M, m;
+
+	for (H = 0; H <= 2; H++)
+	{
+		for (h = 0; h <= 9; h++)
+		{
+			for (M = 0; M < 6; M++)
+
