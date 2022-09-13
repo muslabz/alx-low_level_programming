1aa092cd4e33a102e58cc6d39674869ba3ef2594
muslabz <ajetunmobimust3@gmail.com>
2022-09-13 07:46:45 -0700

    d

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,19 +2,16 @@
 
 /**
  * islower - check lowercase character.
- *
  * @c: integer 1 or 0
  *
  * Return: 1 lowercase 0 Not
  */
-
 int _islower(int c)
 {
 	if (c > 96 && c < 123)
 	{
 		return (1);
-	}
-	else 
+	}else 
 	{
 		return (0);
 	}
