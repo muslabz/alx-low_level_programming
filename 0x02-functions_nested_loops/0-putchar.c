00a68b3e77087dad8db84b1c44f20aa9574ee882
muslabz <ajetunmobimust3@gmail.com>
2022-09-13 00:25:31 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - Entry point
  *
@@ -11,8 +12,10 @@ int main(void)
 	int ch;
 
 	for (ch = 0; ch < 8; ++ch)
+	{
 		_putchar(str[ch]);
-	
+	}
+
 	_putchar('\n');
 
 	return (0);
