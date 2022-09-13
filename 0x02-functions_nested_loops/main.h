3f1aebe4f086a28abe000ba96008f21984454fbb
muslabz <ajetunmobimust3@gmail.com>
2022-09-13 04:52:45 -0700

    s

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+int _islower(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+void print_times_table(int n);
+void print_to_98(int n);
+int add(int, int);
+int print_last_digit(int);
+int _abs(int);
+int print_sign(int n);
+int _isalpha(int c);
