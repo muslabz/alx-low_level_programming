78718ad919c2f7b6bfe0a11753b8cb34dd421dca
muslabz <ajetunmobimust3@gmail.com>
2022-09-15 13:31:14 -0700

    f

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,15 +6,17 @@
  * Return: void
  */
 
-void print_square(int z)
+void print_square(int size)
 {
 	int i, j;
 
-	if (z <= 0)
-		_putchar('\n');
-	for (i = 0; i < z; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < (z); j++)
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < (size); j++)
 		{
 			_putchar('#');
 		}
