efef8364c0da7769ed5f18474eaba08a12f7f450
muslabz <ajetunmobimust3@gmail.com>
2022-09-19 10:20:15 -0700

    f

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * rev_string - prints a string in reverse
+ * @s: the used string to beb reversed
+ * Return 0;
+ */
+
+void rev_string(char *s)
+{
+	int lnt, k, half;
+	char fuss;
+
+	for (lnt = 0; s[lnt] != '\0' ; len++)
+	{
+		k = 0;
+		half = lnt / 2 ;
+
+		while (half--)
+		{
+			fuss = s[lnt -k - 1];
+			s[lnt -k - 1] = s[k];
+
+			s[k] = fuss;
+			k++;
+		}
+	}
+}
+
+
+
+
