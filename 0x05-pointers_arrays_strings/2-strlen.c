d08dc2706e89b77e4cbf98b47ecb3039c97d4184
muslabz <ajetunmobimust3@gmail.com>
2022-09-19 09:42:55 -0700

    f

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of a string
+ * @s: string to check
+ * Return: int length of the string
+ */
+
+int _strlen(char *s)
+{
+	int lnt = 0;
+	while (s[lnt ])
+		lnt++;
+	return (lnt);
+}
+
+
