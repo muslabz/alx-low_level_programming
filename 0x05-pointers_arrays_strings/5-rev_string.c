0fa63a8d2c41e388e0f67dde754365c5bbcd4636
muslabz <ajetunmobimust3@gmail.com>
2022-09-19 10:29:44 -0700

    d

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,12 +14,12 @@ void rev_string(char *s)
 	for (lnt = 0; s[lnt] != '\0' ; lnt++)
 	;
 	k = 0;
-	half = lnt / 2 ;
+	half = lnt / 2;
 
 	while (half--)
 	{
-		fuss = s[lnt -k - 1];
-		s[lnt -k - 1] = s[k];
+		fuss = s[lnt - k - 1];
+		s[lnt - k - 1] = s[k];
 		s[k] = fuss;
 		k++;
 	}
