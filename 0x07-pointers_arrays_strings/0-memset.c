907c0a9bbdd3418565878ec21c721f239251f6e5
muslabz <ajetunmobimust3@gmail.com>
2022-09-26 15:26:08 -0700

    f

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	
-	for (index = 0; n > 0; index++, n--)
-	{
-		s[index] = b;
-	}
+	unsigned int i;
 
-	return (s);
+	for (i = 0; i < n; ++i)
+	{
+		s[i] = b;
+		return (s);
+	}
 }
