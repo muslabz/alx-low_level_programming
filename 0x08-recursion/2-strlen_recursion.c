19937b6847fe562844fc5fc35d9f119a97444a71
muslabz <ajetunmobimust3@gmail.com>
2022-09-28 08:49:59 -0700

    f

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: function parameter
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i = strlen(s);
+
+	return (i)
+}
+
