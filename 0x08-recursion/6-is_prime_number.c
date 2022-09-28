55f54d29602cb45b5f0281dd080522479f214403
muslabz <ajetunmobimust3@gmail.com>
2022-09-28 09:13:50 -0700

    g

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_prime_number - returns a prime numvber
+ * @n: function parameter
+ * Return: 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+	{
+		return (0);
+	}
+	return (n % 2 == 1);
+}
+
