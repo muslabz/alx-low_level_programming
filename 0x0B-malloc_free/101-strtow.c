1b6c3efa98c6566f1a8c62f151ffdb4a14032a05
muslabz <ajetunmobimust3@gmail.com>
2022-10-04 07:42:24 -0700

    g

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 
-int word_len(char *str);
-int count_words(char *str);
-char **strtow(char *str);
-
 /*
  * word_len - Locates the index marking the end of the first word contained within a string.
  * @str: The string to be searched.
  * Return: The index marking the end of the initial word pointed to by str.
  */
+
+int word_len(char *str);                                                                                                              
+int count_words(char *str);                                                                                                           
+char **strtow(char *str);  
 
 int word_len(char *str)
 {
@@ -51,7 +51,8 @@ int count_words(char *str)
 /**
  * strtow - Splits a string into words.
  * @str: The string to be split.
- * Return: If str = NULL, str = "", or the function fails - NULL. Otherwise - a pointer to an array of strings (words).
+ * Return: If str = NULL, str = "", or the function fails - NULL. 
+ * 	   Otherwise - a pointer to an array of strings (words).
  */
 
 char **strtow(char *str)
@@ -63,10 +64,12 @@ char **strtow(char *str)
 		return (NULL);
 	
 	words = count_words(str);
+
 	if (words == 0)
 		return (NULL);
 
 	strings = malloc(sizeof(char *) * (words + 1));
+
 	if (strings == NULL)
 		return (NULL);
 
