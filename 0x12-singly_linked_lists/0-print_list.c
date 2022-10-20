10a903280844a042cba0a80b32f0bf9651d02d31
muslabz <ajetunmobimust3@gmail.com>
2022-10-20 05:04:20 -0700

    g

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,18 +9,17 @@
 
 size_t print_list(const list_t *h)
 {
-	unsigned int nodes = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		printf("[%u] ", h->len);
 		if (h->str == NULL)
-			printf("(nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf("%s\n", h->str);
-
-		h = h->next;
+			printf("[%d] %s\n", h->len, h->str);
+	
 		nodes++;
+		h = h->next; 
 	}
 	return (nodes);
 }
