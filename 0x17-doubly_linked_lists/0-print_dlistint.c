9d242ecd4cf7c6f24ad1a6770ef4c927fc58a463
muslabz <ajetunmobimust3@gmail.com>
2022-12-08 11:38:40 -0800

    jvgfxsdtrf

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,14 +8,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t c = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		c++;
 	}
-	return (c);
+
+	return (nodes);
 }
 
